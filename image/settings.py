d54f8bf7dfebef8d50cbc941c9c44c6ada688520
s.konishi <kakikukekonisi2599@gmail.com>
2020-12-09 21:33:39 +0900

    debun true

--- image/settings.py
+++ image/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # ---
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False#デプロイ用
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
